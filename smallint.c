3b67429fab94bfd3aa591509622218c43bab1a78
caio <caio-valente@hotmail.com>
2015-09-21 00:15:18 -0300

    configura overflow

--- smallint.c
+++ smallint.c
@@ -1,47 +1,40 @@
  /* Caio Valente 1313402 3WB */
  /* Ilana Segal 1510329 3WB */
 
-
 #include "smallint.h"
 #include <stdio.h>
 #include <math.h>
 #define FLOWSIZE 32
 
+typedef unsigned VetSmallInt;
+
 // verifica se um inteiro é overflow e retorna 1 se sim
 int overflow (int a) {
-        return ( a>(FLOWSIZE-1) || a< -FLOWSIZE )
+	return ( a>(FLOWSIZE-1) || a< -FLOWSIZE )
 }
 
-// colocar o boolean do overflow do vetor
+
+
+// coloca o boolean do overflow do vetor
 setOverflow (int index, int status, int *v) {
-        v[i]=status;
+	int bitOfOverflow, overflowInbinary, i;
+	if (status) {
+		bitOfOverflow = VECTORSIZE - (pow(2,index)+1); // calcula o numero do bit que deve ser sobrescrito
+		overflowInbinary = pow(2,bitOfOverflow); // gera um numero binario com apenas um bit true (igual a 1)
+		v = (v | overflowInbinary);
+	}
 }
-
 
 VetSmallInt vs_new(int val[])
 {
-	
-	int i, n;
-	VetSmallInt siv[];
-	
-	for (n=24; n<28; n++)
-	{
-		v[n] = 0;
-	}
-	
-	for (i = 0; i<4; i++)
-	{
-		if (v[i] < 2^5)
-			{
-				siv[31-i] = 0;
-				siv[i] = v[i]
-			}
-		else
-			{
-				siv[31-i] = 1;
-				//siv[i] = 
-			}
-	}
-	
-	return siv;
+        int i, n;
+        VetSmallInt siv; // é o vetor de pequenos inteiros
+
+		// configura os overflow
+        for (i = 0; i<4; i++)
+			setOverflow(i, overflow(val[i]), &siv);
+
+
+
 }
+
