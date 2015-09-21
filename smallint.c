8758b7d848a41208367eb15cb7d365409971df7f
Ilana <ilanaasegal@gmail.com>
2015-09-20 23:12:05 -0300

    comecando funcao vs_new

--- smallint.c
+++ smallint.c
@@ -3,9 +3,33 @@
 
 
 #include "smallint.h"
+#include <stdio.h>
+#include <math.h>
 
 VetSmallInt vs_new(int val[])
 {
-  printf ("ILANA LINDA");
+	
+	int i, n;
+	VetSmallInt siv[];
+	
+	for (n=24; n<28; n++)
+	{
+		v[n] = 0;
+	}
+	
+	for (i = 0; i<4; i++)
+	{
+		if (v[i] < 2^5)
+			{
+				siv[31-i] = 0;
+				siv[i] = v[i]
+			}
+		else
+			{
+				siv[31-i] = 1;
+				//siv[i] = 
+			}
+	}
+	
+	return siv;
 }
-
