7d726fd9fadc215cc3f6df181939640a04f30ee3
caio <caio-valente@hotmail.com>
2015-09-21 01:35:38 -0300

    adiciona funcao que preenche os zeros

--- smallint.c
+++ smallint.c
@@ -4,37 +4,44 @@
 #include "smallint.h"
 #include <stdio.h>
 #include <math.h>
-#define FLOWSIZE 32
+#define FLOWSIZE 32 // 2^5 é o modulo maximo pra 6 bits signed
+#define VECTORSIZE 32
 
 typedef unsigned VetSmallInt;
 
-// verifica se um inteiro é overflow e retorna 1 se sim
+// verifica se um inteiro vai truncar na conversao e retorna 1 se sim
 int overflow (int a) {
 	return ( a>(FLOWSIZE-1) || a< -FLOWSIZE )
 }
 
-
-
-// coloca o boolean do overflow do vetor
-setOverflow (int index, int status, int *v) {
+// coloca o boolean de overflow de um determinado indice do vetor no respectivo bit do SIV
+setOverflow (int index, int status, VetSmallInt *v) {
 	int bitOfOverflow, overflowInbinary, i;
 	if (status) {
-		bitOfOverflow = VECTORSIZE - (pow(2,index)+1); // calcula o numero do bit que deve ser sobrescrito
+		bitOfOverflow = VECTORSIZE - (index+1); // calcula o numero do bit que deve ser sobrescrito
 		overflowInbinary = pow(2,bitOfOverflow); // gera um numero binario com apenas um bit true (igual a 1)
 		v = (v | overflowInbinary);
 	}
 }
 
+// preenche os bits 24 a 27 com zeros (e nao altera os outros)
+fillWithZeros (VetSmallInt *a) {
+	*a = (*a & 0xFFFFFF0F);	
+}
+
 VetSmallInt vs_new(int val[])
 {
         int i, n;
-        VetSmallInt siv; // é o vetor de pequenos inteiros
-
-		// configura os overflow
-        for (i = 0; i<4; i++)
-			setOverflow(i, overflow(val[i]), &siv);
-
-
-
+	VetSmallInt siv; // é o vetor de pequenos inteiros
+	
+	// preenche os bits 24 a 27
+	fillWithZeros(&siv);
+	
+	// configura os overflow nos bits 28 a 31
+	for (i = 0; i<4; i++)
+		setOverflow(i, overflow(val[i]), &siv);
+	
+	
+	
+	return siv;
 }
-
