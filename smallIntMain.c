f5a4a7f4bf45ec295f067895495a45c5499bdbbc
valentecaio <caio-valente@hotmail.com>
2015-09-24 16:28:25 -0300

    chmod +x *

--- smallIntMain.c
+++ smallIntMain.c
@@ -11,22 +11,18 @@ void printa_vetor (int *a, int n) {
 }
 
 int main (void) {
-	int a[VECTORSIZE] = {4, 20, -30, 60};
-	int b[VECTORSIZE] = {4, 20, -30, 60};
+	int a[VECTORSIZE] = {4, 20, -4, 50};
 	VetSmallInt v;
 	
 	printa_vetor(a, VECTORSIZE);
-	printa_vetor(b, VECTORSIZE);
 	
 	// teste de criação e print
 	printf ("\n\n------TESTE DE CRIACAO------\n\n");
 	v = vs_new(a);
 	vs_print(v);
-	v = vs_new(b);
-	vs_print(v);
 	
 	printf ("\n\n------TESTE DE SOMA------\n\n");
-	v = vs_add(vs_new(a), vs_new(b));
+	v = vs_add(vs_new(a), vs_new(a));
 	vs_print(v);
 	
 	return 0;
