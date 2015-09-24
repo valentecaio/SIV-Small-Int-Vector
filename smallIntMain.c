03f981119c2c119e196e0a605f005f5d82aa07b4
valentecaio <caio-valente@hotmail.com>
2015-09-24 12:41:56 -0300

    adiciona arquivo de main

---
+++ smallIntMain.c
@@ -0,0 +1,17 @@
+
+#include "smallint.h"
+
+void printa_vetor (int *a, int n) {
+	int i;
+	printf ("\nPrintando dados do vetor de inteiros:\n");
+	for (i=0; i<n; i++) {
+		printf ("\nentrada %d \t valor: %d", i, a[i]);
+	}	
+}
+
+int main (void) {
+	int a[VECTORSIZE] = {4, 10, -20, 60};
+	printa_vetor(a, VECTORSIZE);
+	VetSmallInt v = vs_new(a);
+	vs_print(a);
+}
