0981f27bb3b7dd9261728082a22e2f74270ac13e
valentecaio <caio-valente@hotmail.com>
2015-09-25 23:48:31 -0300

    header final

--- smallint.h
+++ smallint.h
@@ -11,23 +11,20 @@ VetSmallInt vs_add(VetSmallInt v1, VetSmallInt v2);
 /* Imprime um vetor de Small Int */
 void vs_print(VetSmallInt v);
 
-/* Shift lÃ³gico para a direita */
+/* Shift logico para a direita */
 VetSmallInt vs_shr(VetSmallInt v, int n);
 
-/* Shift aritmÃ©tico para a direita */
+/* Shift aritmetico para a direita */
 VetSmallInt vs_sar(VetSmallInt v, int n);
 
 /* Shift para a esquerda */
 VetSmallInt vs_shl(VetSmallInt v, int n);
 
-/* LÃª um vetor de small ints */
+/* Le um vetor de small ints */
 VetSmallInt vs_read(FILE *f);
 
 /* Grava um vetor de small ints */
 int vs_write(VetSmallInt v, FILE *f);
-
-/* Grava um vetor de small ints em binario */
-int vs_writeInBinary(VetSmallInt v, FILE *f);
 
 /* pega o small int do indice index e devolve como signed int */
 int vs_get (VetSmallInt *v, int index);
@@ -36,4 +33,4 @@ int vs_get (VetSmallInt *v, int index);
 void vs_set (int index, int x, VetSmallInt *v);
 
 /* printa um vetor de small int em binario */
-void vs_printInBinary (int n); 
+void vs_printInBinary (int n);
